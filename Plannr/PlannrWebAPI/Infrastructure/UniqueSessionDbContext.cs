812ddd3f91b2efa824204887fab5f6fffe217134
mmamel <43307993+mmamel@users.noreply.github.com>
2023-11-26 21:49:14 -0800

    blazor -> react + calendar component

--- Plannr/PlannrWebAPI/Infrastructure/UniqueSessionDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Plannr.SharedModels;
-
-namespace PlannrWebAPI.Infrastructure;
-
-public class UniqueSessionDbContext(DbContextOptions<UniqueSessionDbContext> options) : DbContext(options)
-{
-    public DbSet<UniqueSession> UniqueSessions => Set<UniqueSession>();
-}

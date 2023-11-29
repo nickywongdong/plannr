2bb375513e98bb69a10b65143105a7555251a19c
mmamel <43307993+mmamel@users.noreply.github.com>
2023-11-28 19:30:21 -0800

    idk

--- src/plannr.client/src/Components/PlannrWrapper/PlannrWrapper.jsx
+++ src/plannr.client/src/Components/PlannrWrapper/PlannrWrapper.jsx
@@ -2,12 +2,14 @@ import './index.css'
 import PlannrCalendar from '../PlannrCalendar'
 import ModeToggle from '../ModeToggle'
 import { useState } from 'react'
+import InfoModal from '../InfoModal'
 
 const PlannrWrapper = () => {
     const [isEditMode, setEditMode] = useState(true)
     return (
         <div className='plannr-wrapper'>
-            <ModeToggle isEditMode={isEditMode} setEditMode={setEditMode}/>
+            <ModeToggle isEditMode={isEditMode} setEditMode={setEditMode} />
+            <InfoModal />
             <PlannrCalendar isEditMode={isEditMode}/>
         </div>
         

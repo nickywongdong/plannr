fa7b1524365a649379e84a752ce3952066933b06
nickywongdong <wongnich@gmail.com>
2023-11-29 20:00:51 -0800

    Adding footer with github action workflow badges

--- src/plannr.client/src/App.jsx
+++ src/plannr.client/src/App.jsx
@@ -1,22 +1,29 @@
-import './App.css';
+// App.jsx
+
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
-import { ColorModeProvider } from './Contexts/ColorModeContext';
+import Box from '@mui/material/Box';
 
+import { ColorModeProvider } from './Contexts/ColorModeContext';
 import PlannrWrapper from './Components/PlannrWrapper';
 import NavBar from './Components/NavBar';
+import AppFooter from './Components/AppFooter/AppFooter';
+
+import './App.css'; // Import your CSS file
 
 function App() {
-
     return (
         <React.Fragment>
             <ColorModeProvider>
-                <CssBaseline enableColorScheme/>
-                <NavBar />
-                <Container maxWidth="sm">
-                    <PlannrWrapper />
-                </Container>
+                <CssBaseline enableColorScheme />
+                <Box className="app-container">
+                    <NavBar />
+                    <Container className="App" component="main">
+                        <PlannrWrapper />
+                    </Container>
+                    <AppFooter />
+                </Box>
             </ColorModeProvider>
         </React.Fragment>
     );
--- src/plannr.client/src/Components/NavBar/NavBar.jsx
+++ src/plannr.client/src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 import ThemeToggleButton from '../ThemeToggleButton';
 
@@ -12,6 +12,9 @@ export default function NavBar() {
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Plannr
+                        </Typography>
                         <ThemeToggleButton />
                     </Toolbar>
                 </AppBar>
